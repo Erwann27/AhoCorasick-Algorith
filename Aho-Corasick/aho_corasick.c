79f1db17d7835b1f0de1d404bdab40d129083283
Zounkla <flo-vl@orange.fr>
2023-11-09 11:40:43 +0100

    début algo AC (squelette)

---
+++ Aho-Corasick/aho_corasick.c
@@ -0,0 +1,9 @@
+#include "aho_corasick.h"
+
+size_t aho_corasick(Trie trie, size_t word_count, char *text, size_t text_length) {
+    trie = trie;
+    word_count = word_count;
+    text = text;
+    text_length = text_length;
+    return 0;
+}
