61d6463d80ad0352745270d4c6ccfcfbab29840b
Erwann27 <montagne.erwann.2@gmail.com>
2023-11-15 21:12:35 +0100

    fonction pre-ac

--- Aho-Corasick/aho-corasick.c
+++ Aho-Corasick/aho-corasick.c
@@ -7,3 +7,17 @@ size_t aho_corasick(Trie trie, size_t word_count, char *text, size_t text_length
     text_length = text_length;
     return 0;
 }
+
+Trie pre_ac(char **word_list, size_t word_count){
+    Trie trie = createTrie(100);
+    for(size_t i = 0; i < word_count; i += 1){
+        insertInTrie(trie, (unsigned char*)word_list[i]);
+    }
+    for(unsigned char a = 0; a < UCHAR_MAX; a += 1){
+        if(!is_transition(trie, 0, (char)a)){
+            create_transition(trie, 0, (char)a, 0);
+        }
+    }
+    // complete(0);
+    return trie;
+}
