efd4d501b12c40e61fbfc464cef8eefcbc5a1fdf
Florian Van Liedekerke <florian.van-liedekerke@univ-rouen.fr>
2023-11-30 20:05:08 +0100

    fixed partially research.sh

--- Scripts/research.sh
+++ Scripts/research.sh
@@ -7,27 +7,27 @@ do
 y=../Texts/texts_5000000_$j.txt
 file=../Texts/words_100_5_15_$j.txt
 echo "mat 5 15 $j:" >> $result
-(time ac-matrice $file $y) 2>> $result
+(time ../ac-matrice $file $y) 2>> $result
 
 file=../Texts/words_100_15_30_$j.txt
 echo "mat 15 30 $j :" >> $result
-(time ac-matrice $file $y) 2>> $result
+(time ../ac-matrice $file $y) 2>> $result
 
 file=../Texts/words_100_30_60_$j.txt
 echo "mat 30 60 $j :" >> $result
-(time ac-matrice $file $y) 2>> $result
+(time ../ac-matrice $file $y) 2>> $result
 
 file=../Texts/words_100_5_15_$j.txt
 echo "hash 5 15 $j:" >> $result
-(time ac-hachage $file $y) 2>> $result
+(time ../ac-hachage $file $y) 2>> $result
 
 file=../Texts/words_100_15_30_$j.txt
 echo "hash 15 30 $j :" >> $result
-(time ac-hachage $file $y) 2>> $result
+(time ../ac-hachage $file $y) 2>> $result
 
 file=../Texts/words_100_30_60_$j.txt
 echo "hash 30 60 $j :" >> $result
-(time ac-hachage $file $y) 2>> $result
+(time ../ac-hachage $file $y) 2>> $result
 
 
 done
